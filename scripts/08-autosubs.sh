4ab03286774a64e23147b7bfd93b76739e1fad48
saul <saul@localhost.localdomain>
2026-07-24 20:37:42 -0300

    Improve openSUSE setup and AutoSubs installation

--- scripts/08-autosubs.sh
+++ scripts/08-autosubs.sh
@@ -51,6 +51,7 @@ echo "Instalando dependencias de AutoSubs..."
 
 sudo zypper install -y \
 libwebkit2gtk-4_1-0
+Mesa-libgbm
 
 echo
 echo "Instalando AutoSubs..."
@@ -60,7 +61,30 @@ sudo rpm -i --nodeps "$INSTALLER"
 echo
 echo "Actualizando caché de librerías..."
 
+
 sudo ldconfig
+
+echo
+echo "Aplicando workaround para WebKitGTK (pantalla negra)..."
+
+if [[ -f /usr/bin/autosubs && ! -f /usr/bin/autosubs.real ]]; then
+    sudo mv /usr/bin/autosubs /usr/bin/autosubs.real
+
+    sudo tee /usr/bin/autosubs >/dev/null <<'EOF'
+#!/usr/bin/env bash
+
+export GDK_BACKEND=x11
+export WEBKIT_DISABLE_DMABUF_RENDERER=1
+
+exec /usr/bin/autosubs.real "$@"
+EOF
+
+    sudo chmod +x /usr/bin/autosubs
+
+    echo "Workaround aplicado."
+else
+    echo "Workaround ya aplicado."
+fi
 
 echo
 echo "Verificando instalación..."
